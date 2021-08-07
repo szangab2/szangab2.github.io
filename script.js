5f8d413de50ef12571d7cfc394ea8207720596a5
szangab2 <nk.gabor.szanto@gmail.com>
2021-08-07 12:00:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let data = {
   };
   let data1 = {
     photo: './pict/house1.jpg',
-    title: 'My title',
-    description: 'What happened here, why is this a very nice image'
+    title: 'A sziklára épült szürke ház',
+    description: 'Ez a ház nekem úgy tűnik fel, mintha erős belövések után lennénk... After heavy artillery fire..'
   };
-  $('#photo').attr('src', data.photo);
-  $('#photo-title').text( data.title);
-  $('#photo-description').text(data.description) ;
+  $('#photo').attr('src', data1.photo);
+  $('#photo-title').text( data1.title);
+  $('#photo-description').text(data1.description) ;
