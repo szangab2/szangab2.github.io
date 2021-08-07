c516e95f3b2bf79d2a0b207fd62c82d6e2408cca
szangab2 <nk.gabor.szanto@gmail.com>
2021-08-07 11:55:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let data = {
     photo: './pict/house0.jpg',
-    title: 'A ház képe',
-    description: 'A ház leírásának helye...'
+    title: 'A cseréptetős templom képe',
+    description: 'A cseréptetős  templomban kevesen tudnak imádkozni az úrhoz..'
+  };
+  let data1 = {
+    photo: './pict/house1.jpg',
+    title: 'My title',
+    description: 'What happened here, why is this a very nice image'
   };
   $('#photo').attr('src', data.photo);
   $('#photo-title').text( data.title);
   $('#photo-description').text(data.description) ;
-  
