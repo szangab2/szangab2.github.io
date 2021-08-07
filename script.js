580c405e79b9c8e03c0981135d6bf1935c897af2
szangab2 <nk.gabor.szanto@gmail.com>
2021-08-07 11:34:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ let data = {
     description: 'A ház leírásának helye...'
   };
   $('#photo').attr('src', data.photo);
+  $('#photo-title').text( data.title);
+  $('#photo-description').text(data.description) ;
+  
