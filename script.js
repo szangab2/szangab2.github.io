c541b89f9e4c0e5aa3e01d6e3e727d9cacbe9819
szangab2 <nk.gabor.szanto@gmail.com>
2021-08-07 12:23:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let data = {
+let data0 = {
     photo: './pict/house0.jpg',
     title: 'A cseréptetős templom képe',
     description: 'A cseréptetős  templomban kevesen tudnak imádkozni az úrhoz..'
@@ -8,8 +8,16 @@ let data = {
     title: 'A sziklára épült szürke ház',
     description: 'Ez a ház nekem úgy tűnik fel, mintha erős belövések után lennénk... After heavy artillery fire..'
   }; 
+  let data2 = {
+    photo: './pict/house2.jpg',
+    title: 'A kék tetős ház',
+    description: 'Tipikus amerikai családi ház... Ja, figyeld meg, focipálya van a ház mögött... Mint Bőrkészítő Futsal Petikéénél..'
+  };
+
+
+
   let currentPhoto = 0;
-let imagesData = [data,data1];
+let imagesData = [data0,data1, data2];
   let loadPhoto = (photoNumber) => {
     $('#photo').attr('src', imagesData[photoNumber].photo);
     $('#photo-title').text(imagesData[photoNumber].title);
